e63db9c7da0418642bad37f001a9925c3c253133
domeniqo <dominik.mlynka@gmail.com>
2020-12-12 21:32:13 +0100

    debugging - getAllOrders() implemented in ordersFacade

---
+++ PhotoBooth.BL/Facades/OrdersFacade.cs
@@ -0,0 +1,82 @@
+﻿using PhotoBooth.BL.Models.Item.Product;
+using PhotoBooth.BL.Models.Item.RentalItem;
+using PhotoBooth.BL.Models.Order;
+using PhotoBooth.BL.Queries;
+using Riganti.Utils.Infrastructure.Core;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using PhotoBooth.DAL.Entity;
+using PhotoBooth.DAL.Repository;
+
+namespace PhotoBooth.BL.Facades
+{
+    public class OrdersFacade : FacadeBase<Order>, IOrderFacade
+    {
+        public OrdersFacade(BaseRepository<Order> repository, IUnitOfWorkProvider uow) : base(repository, uow)
+        {
+
+        }
+     
+        public bool CancelOrder(Guid orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderSummaryModel ChangeOrderPrice(Guid id, double newPrice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ConfirmOrder(Guid orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderSummaryModel CreateOrder(OrderSummaryModel order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteOrder(Guid orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<OrderListModel> GetAllOrders()
+        {
+            return new OrderListQuery(base.UnitOfWorkFactory).Execute();
+        }
+
+        public OrderSummaryModel GetOrderById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<OrderListModel> GetOrdersByUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderSummaryModel GetOrderSummary(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderSummaryModel PrepareOrder(ICollection<RentalItemModel> rentalItems, ICollection<ProductModel> products, OrderMatadata orderMatadata)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderSummaryModel SubmitOrder(ICollection<RentalItemModel> rentalItems, ICollection<ProductModel> products, OrderMatadata orderMatadata)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateOrder(OrderSummaryModel order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

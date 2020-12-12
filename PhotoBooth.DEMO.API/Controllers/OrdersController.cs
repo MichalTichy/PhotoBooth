99a02287da18e4d1c7552a0a4a73542e8a4aa582
domeniqo <dominik.mlynka@gmail.com>
2020-12-12 15:16:50 +0100

    OrdersController used instead of OrderController, get products and product by Id implemented

---
+++ PhotoBooth.DEMO.API/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PhotoBooth.BL.Facades;
+using PhotoBooth.BL.Models.Item.Product;
+using PhotoBooth.BL.Models.Item.RentalItem;
+using PhotoBooth.BL.Models.Order;
+
+
+namespace PhotoBooth.DEMO.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderFacade _orderFacade;
+
+
+        public OrdersController(IOrderFacade orderFacade)
+        {
+            _orderFacade = orderFacade;
+        }
+
+        public async Task<ActionResult<OrderListModel>> Get()
+        {
+            //API ready to work asynchronously when async methods are implemented in lower levels
+            var orders = await Task.Run(() => _orderFacade.GetAllOrders());
+            return Ok(orders);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderSummaryModel>> GetOrderById(Guid id)
+        {
+            var order = await Task.Run(() => _orderFacade.GetOrderById(id));
+            if(order == null)
+            {
+                BadRequest();
+            }
+            return Ok(order);
+        }
+    }
+}

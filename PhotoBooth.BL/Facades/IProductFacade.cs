e3370cab556ed721f0f883f8a7551f8cb0ad0a5a
domeniqo <dominik.mlynka@gmail.com>
2020-12-12 20:03:23 +0100

    ProductsController implemented, IProductFacade created

---
+++ PhotoBooth.BL/Facades/IProductFacade.cs
@@ -0,0 +1,17 @@
+﻿using PhotoBooth.BL.Models.Item.Product;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoBooth.BL.Facades
+{
+    public interface IProductFacade
+    {
+        Task<IList<ProductModel>> GetAllProductsAsync();
+        Task<ProductModel> GetProductByIdAsync(Guid id);
+        Task<ProductModel> AddProductAsync(ProductModel product);
+        Task<bool> DeleteProductAsync(Guid id);
+        Task<bool> EditProductAsync(ProductModel product);
+    }
+}

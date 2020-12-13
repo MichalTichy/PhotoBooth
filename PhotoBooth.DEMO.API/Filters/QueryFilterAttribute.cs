cd0d3d36ad69134fcb91484c5579ad65bfd67c17
domeniqo <dominik.mlynka@gmail.com>
2020-12-13 16:39:31 +0100

    filter for API calls containing query created

---
+++ PhotoBooth.DEMO.API/Filters/QueryFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoBooth.DEMO.API.Filters
+{
+    //ignores requests containing queries in URI
+    public class QueryFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if(context.HttpContext.Request.Query.Count != 0)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}

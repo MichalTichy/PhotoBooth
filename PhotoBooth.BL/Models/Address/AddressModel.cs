2cdfd692ba232e25ff756433c93071ac8bd58833
domeniqo <dominik.mlynka@gmail.com>
2021-01-12 20:41:15 +0100

    Validation messages updated, regex for phone number added

--- PhotoBooth.BL/Models/Address/AddressModel.cs
+++ PhotoBooth.BL/Models/Address/AddressModel.cs
@@ -5,6 +5,7 @@ namespace PhotoBooth.BL.Models.Address
     public class AddressModel : ModelBase
     {
         [Required(ErrorMessage = "Musíte zadat obec!")]
+        [MinLength(5, ErrorMessage = "Miesto musi mat aspon 5 znakov")]
         public string City { get; set; }
 
         //[Required(ErrorMessage = "Musíte zadat PSČ!")]
